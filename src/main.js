ee2f80a079e4ea89f5f9f00f1a87b12aac00691f
mariaestaloca <mariaestaloca@gmail.com>
2019-04-24 11:58:34 -0300

    secciones agregadas, 1er select

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-/* Manejo del DOM */
+
+function cargar() {
+    var selectNivEduCountry = ["Perú", "Mexico", "Brasil", "Chile"]; //Tu array de provincias
+    var select = document.getElementById("selectNivEduCountry"); //Seleccionamos el select
+    
+    for(var i=0; i < selectNivEduCountry.length; i++){ 
+        var option = document.createElement("option"); //Creamos la opcion
+        option.innerHTML = selectNivEduCountry[i]; //Metemos el texto en la opción
+        select.appendChild(option); //Metemos la opción en el select
+    }
+}
+cargar();
